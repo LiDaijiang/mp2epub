42aa17c60c548e1388847680560a6a74498c54c7
coldriver <34183067+LiDaijiang@users.noreply.github.com>
2017-12-05 14:04:58 +0800

    Update wechat_crawl.py
    发现一个用pc端浏览器打开公众号历史文章的路径

--- src/wechat_crawl.py
+++ src/wechat_crawl.py
@@ -1,7 +1,10 @@
 # coding: utf-8
 import requests
 
-profile_root = "https://mp.weixin.qq.com/mp/profile_ext?action=home&__biz=MzIyOTM3ODAxNA==&scene=124&devicetype=android-23&version=26051633&lang=zh_CN&nettype=WIFI&a8scene=3&pass_ticket=i9hOYLZZef7lSHlRGS5XIh5VAGYkbdC%2BShjvuuTd4bsiNGKCnZFdfBrhsPczOQC7&wx_header=1"
+#发现一个用浏览器获取公众号历史文章的方法
+profile_root = "https://mp.weixin.qq.com/mp/profile_ext?action=home&__biz=MzU3MjE2NzQ4Mg==&scene=124&#wechat_redirect"
+profile_root1 = "https://mp.weixin.qq.com/mp/profile_ext?action=home&__biz=MzI3MTgwNTE0Mg==&scene=124&#wechat_redirect"
+#profile_root = "https://mp.weixin.qq.com/mp/profile_ext?action=home&__biz=MzIyOTM3ODAxNA==&scene=124&devicetype=android-23&version=26051633&lang=zh_CN&nettype=WIFI&a8scene=3&pass_ticket=i9hOYLZZef7lSHlRGS5XIh5VAGYkbdC%2BShjvuuTd4bsiNGKCnZFdfBrhsPczOQC7&wx_header=1"
 
 sub_url = "http://mp.weixin.qq.com/s?__biz=MzIyOTM3ODAxNA==&mid=2247491183&idx=2&sn=d1bd8baf699213a947431a94f49b0ec3&chksm=e842ccdedf3545c8a73f771bc0770610ec040445467076d83cf609240e5b507af89dcaa38365&scene=38"
 
